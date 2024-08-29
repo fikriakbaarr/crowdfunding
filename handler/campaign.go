b36f6ee7685abf211f03333619585ea6c93d63c8
fikriakbaarr <fapermadi@gmail.com>
2024-08-29 20:43:42 +0700

    get campaign detail endpoint

--- handler/campaign.go
+++ handler/campaign.go
@@ -29,3 +29,23 @@ func (h *campaignHandler) GetCampaigns(c *gin.Context) {
 	response := helpher.APIResponse("List of campaign", http.StatusOK, "success", campaign.FormatCampaigns(campaigns))
 	c.JSON(http.StatusOK, response)
 }
+
+func (h *campaignHandler) GetCampaign(c *gin.Context) {
+	var input campaign.GetCampaignDetailInput
+
+	err := c.ShouldBindUri(&input)
+	if err != nil {
+		response := helpher.APIResponse("Failed to get detail of campaign", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+	campaignDetail, err := h.service.GetCampaignByID(input)
+	if err != nil {
+		response := helpher.APIResponse("Failed to get detail of campaign", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response := helpher.APIResponse("Campaign detail", http.StatusOK, "success", campaign.FormatCampaignDetail(campaignDetail))
+	c.JSON(http.StatusOK, response)
+}
