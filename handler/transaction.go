b32b4e6fc01bd7d229e839c2e201ca640320ed7c
fikriakbaarr <fapermadi@gmail.com>
2024-09-12 12:28:09 +0700

    endpoint list of  transaction

--- handler/transaction.go
+++ handler/transaction.go
@@ -1,1 +1,43 @@
 package handler
+
+import (
+	"crowdfunding/helpher"
+	"crowdfunding/transaction"
+	"crowdfunding/user"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type transactionHandler struct {
+	service transaction.Service
+}
+
+func NewTransactionHandler(service transaction.Service) *transactionHandler {
+	return &transactionHandler{service}
+}
+
+func (h *transactionHandler) GetCampaignTransactions(c *gin.Context) {
+	var input transaction.GetCampaignTransactionsInput
+
+	err := c.ShouldBindUri(&input)
+	if err != nil {
+		response := helpher.APIResponse("Failed to get campaign's transactions", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	currentUser := c.MustGet("currentUser").(user.User)
+
+	input.User = currentUser
+
+	transactions, err := h.service.GetTransactionCampaignID(input)
+	if err != nil {
+		response := helpher.APIResponse("Failed to get campaign's transactions", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response := helpher.APIResponse("Campaign transactions", http.StatusOK, "success", transaction.FormatCampaignTransactions(transactions))
+	c.JSON(http.StatusOK, response)
+}
